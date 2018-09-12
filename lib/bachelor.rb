e5ba5bcbc4ebc463240ae537df820cedbf693aa7
Nico-ehs <nico.ehs05@gmail.com>
2018-09-12 16:43:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  r=data[season].find{|el| el["status"]=="Winner"}.split(" ").[0]
+  r=data[season].find{|el| el["status"]=="Winner"}
 end
 
 def get_contestant_name(data, occupation)
